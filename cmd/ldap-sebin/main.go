c4a6afc8b487fe3854171f1783e1a276fc3b1238
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2025-08-19 23:44:30 +0200

    Refactor LDAP stack, add TLS server support
    client operations:
    - switch to better maintained library
    - replace deprecated dial function and error prone manual cipher
      handling
    - refactor configuration to take URL instead of individual address and
      port, simplifying parsing and aligning with other LDAP clients
    
    server operations:
    - switch to library with less need for manual features
    - implement TLS listener to encourage secure communications
    - allow for customization of listen address and port through
      configuration
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- cmd/ldap-sebin/main.go
+++ cmd/ldap-sebin/main.go
@@ -6,16 +6,18 @@ import (
 
 	"os"
 
-	"beryju.io/ldap"
 	"github.com/josegomezr/ldap-sebin/internal/handler"
+	"github.com/merlinz01/ldapserver"
 	"gopkg.in/yaml.v3"
 	"sync"
 )
 
 type Cfg struct {
-	Server         string `yaml:"server"`
-	TLS            bool   `yaml:"tls"`
-	Port           int    `yaml:"port"`
+	Upstream       string `yaml:"upstream-server-url"`
+	ListenAddr     string `yaml:"listen-address"`
+	ListenPort     int    `yaml:"listen-port"`
+	Certificate    string `yaml:"certificate"`
+	Key            string `yaml:"key"`
 	BaseDn         string `yaml:"base-dn"`
 	FilterTemplate string `yaml:"filter-template"`
 }
@@ -33,21 +35,47 @@ func main() {
 	}
 	fmt.Printf("--- cfg:\n%+v\n\n", cfg)
 
-	s := ldap.NewServer()
-
 	handler := &handler.Handler{
 		Sessions:       make(map[string]handler.Session),
-		LdapServer:     cfg.Server,
-		LdapPort:       cfg.Port,
-		Tls:            cfg.TLS,
+		LdapUrl:        cfg.Upstream,
 		BaseDn:         cfg.BaseDn,
 		FilterTemplate: cfg.FilterTemplate,
 		Mutex:          &sync.Mutex{},
 	}
-	s.BindFunc("", handler)
-	s.CloseFunc("", handler)
 
-	if err := s.ListenAndServe("localhost:3388"); err != nil {
-		log.Fatalf("LDAP Server Failed: %s", err.Error())
+	s := ldapserver.NewLDAPServer(handler)
+
+	listenAddr := cfg.ListenAddr
+	if listenAddr == "" {
+		listenAddr = "localhost"
+	}
+
+	var srvErr error
+
+	if cfg.Certificate == "" || cfg.Key == "" {
+		log.Println("No certificate and/or key configured. Listening for plain text connections. This is NOT secure.")
+
+		listenPort := cfg.ListenPort
+		if listenPort == 0 {
+			listenPort = 389
+		}
+
+		srvErr = s.ListenAndServe(fmt.Sprintf("%s:%d", listenAddr, listenPort))
+	} else {
+		err := s.SetupTLS(cfg.Certificate, cfg.Key)
+		if err != nil {
+			log.Fatalf("TLS configuration failed: %s", err)
+		}
+
+		listenPort := cfg.ListenPort
+		if listenPort == 0 {
+			listenPort = 636
+		}
+
+		srvErr = s.ListenAndServeTLS(fmt.Sprintf("%s:%d", listenAddr, listenPort))
+	}
+
+	if srvErr != nil {
+		log.Fatalf("Failed to start LDAP server: %s", srvErr)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/josegomezr/ldap-sebin
 go 1.24.5
 
 require (
-	beryju.io/ldap v0.1.0
+	github.com/go-ldap/ldap/v3 v3.4.11
+	github.com/merlinz01/ldapserver v0.0.16
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require github.com/nmcclain/asn1-ber v0.0.0-20170104154839-2661553a0484 // indirect
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.8-0.20250403174932-29230038a667 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+)
